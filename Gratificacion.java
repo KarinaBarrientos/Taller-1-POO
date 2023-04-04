9095a968ff59f116193c21f648f0bff5e55346a6
KarinaBarrientos <kbbarrientosb@gmail.com>
2023-04-04 12:39:48 -0400

    primer avance gratificación

--- Gratificacion.java
+++ Gratificacion.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
 public class Gratificacion {
+    public static void gratificacion (String args []){
+        int gratmax = 162292;
+
+        Scanner in = new Scanner (System.in);
+
+        String siono = "";
+
+        System.out.println("¿Resives gratificación mensual?");
+        siono = in.nextLine();
+
+        if (siono == "si" || siono == "Si || soino == SI"){
+
+            if (Main.sueldobase <= gratmax ){
+
+
+            }
+
+
+        }
+
+
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
 public class Main {
+    public static int sueldobase;
+    public static void main (String [] args){
+
+        Scanner inbase = new Scanner (System.in);
+
+        System.out.println("¿Cuál es tu sueldo base?");
+        sueldobase = inbase.nextInt();
+
+    }
+
 }
