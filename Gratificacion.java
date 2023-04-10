a93a5f84b4ca7e93cc3af93373b4af2eb1591a02
KarinaBarrientos <kbbarrientosb@gmail.com>
2023-04-10 15:57:42 -0400

    gratificación

--- Gratificacion.java
+++ Gratificacion.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
 
 public class Gratificacion {
-    public static void gratificacion(String args[]) {
-        int gratmax = 162292;
+    public int metodo_gratificacion (int sueldobruto) {
 
-        Scanner in = new Scanner(System.in);
-
-        String siono = "";
-
-        System.out.println("¿Recibes gratificación mensual?");
-        siono = in.nextLine();
-
-        if (siono == "si" || siono == "Si || soino == SI") {
-
-            if (Main.sueldobruto <= gratmax) {
-            }
+        if (sueldobruto > 649168) {
+            sueldobruto = sueldobruto + 162292;
         }
+        else {
+            sueldobruto = (int) (sueldobruto + sueldobruto * 0.25);
+        }
+        return sueldobruto;
     }
+
 }
+
--- Main.java
+++ Main.java
@@ -7,12 +7,26 @@ public class Main {
 
         Scanner inbase = new Scanner(System.in);
 
-        System.out.println("¿Cuál es tu sueldo base?");
+        System.out.println("¿Cuál es tu sueldo bruto?");
         if (inbase.hasNextInt()) {
             sueldobruto = inbase.nextInt();
         } else {
             System.out.println("Formato incorrecto, favor de ingresar solo numeros");
         }
+
+        Gratificacion gratificacion = new Gratificacion();
+        System.out.println("¿Recibe gratificación?");
+        String resive_gratificacion = inbase.next();
+            if (resive_gratificacion.equals("si")) {
+                sueldobruto = (int) (gratificacion.metodo_gratificacion(sueldobruto));
+            }
+            else if (resive_gratificacion.equals("no")) {
+                sueldobruto = sueldobruto;
+            }
+            else {
+                System.out.println("Respuesta invalida, por favor reponder con ´si´ o ´no´");
+            }
+
 
         AFP afp = new AFP(sueldobruto);
         double descuentoafp = afp.operacion();
@@ -22,6 +36,6 @@ public class Main {
 
         int sueldoliquido = (int) (sueldobruto - descuentoafp - descuentosalud);
         //le falta gratificacion
-        System.out.println(sueldoliquido);
+        System.out.println("Su sueldo líquido es: " + sueldoliquido);
     }
 }
