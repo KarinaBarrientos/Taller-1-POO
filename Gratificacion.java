e7f3d0810bc04e6666440aa668532d888696a89c
KarinaBarrientos <kbbarrientosb@gmail.com>
2023-04-13 16:40:13 -0400

    modificación clase Gratificacion

--- Gratificacion.java
+++ Gratificacion.java
@@ -1,16 +1,30 @@
 import java.util.Scanner;
 
 public class Gratificacion {
-    public int metodo_gratificacion (int sueldobruto) {
+    int sueldobruto;
 
-        if (sueldobruto > 649168) {
-            sueldobruto = sueldobruto + 162292;
-        }
-        else {
-            sueldobruto = (int) (sueldobruto + sueldobruto * 0.25);
-        }
-        return sueldobruto;
+    Gratificacion(int sueldobruto) {
+        this.sueldobruto = sueldobruto;
     }
 
+    public int metodo_gratificacion() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¿Recibe gratificación?");
+        String resive_gratificacion = scanner.next();
+        if (resive_gratificacion.equals("si")) {
+            if (this.sueldobruto > 649168) {
+                this.sueldobruto = this.sueldobruto + 162292;
+            } else {
+                this.sueldobruto = (int) (this.sueldobruto + this.sueldobruto * 0.25);
+            }
+        } else if (resive_gratificacion.equals("no")) {
+
+        } else {
+            System.out.println("Respuesta invalida, por favor reponder con ´si´ o ´no´");
+        }
+        return this.sueldobruto;
+
+    }
 }
 
--- Main.java
+++ Main.java
@@ -5,28 +5,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner inbase = new Scanner(System.in);
+        Scanner inbruto = new Scanner(System.in);
 
         System.out.println("¿Cuál es tu sueldo bruto?");
-        if (inbase.hasNextInt()) {
-            sueldobruto = inbase.nextInt();
-        } else {
+        if (inbruto.hasNextInt()) {
+            sueldobruto = inbruto.nextInt();
+        }
+        else {
             System.out.println("Formato incorrecto, favor de ingresar solo numeros");
         }
 
-        Gratificacion gratificacion = new Gratificacion();
-        System.out.println("¿Recibe gratificación?");
-        String resive_gratificacion = inbase.next();
-            if (resive_gratificacion.equals("si")) {
-                sueldobruto = (int) (gratificacion.metodo_gratificacion(sueldobruto));
-            }
-            else if (resive_gratificacion.equals("no")) {
-                sueldobruto = sueldobruto;
-            }
-            else {
-                System.out.println("Respuesta invalida, por favor reponder con ´si´ o ´no´");
-            }
-
+        Gratificacion gratificacion = new Gratificacion(sueldobruto);
+        sueldobruto = gratificacion.metodo_gratificacion();
 
         AFP afp = new AFP(sueldobruto);
         double descuentoafp = afp.operacion();
@@ -34,8 +24,8 @@ public class Main {
         Isaprefonasa eleccionfonasaoisapre = new Isaprefonasa(sueldobruto);
         double descuentosalud = eleccionfonasaoisapre.operacion();
 
-        int sueldoliquido = (int) (sueldobruto - descuentoafp - descuentosalud);
-        //le falta gratificacion
-        System.out.println("Su sueldo líquido es: " + sueldoliquido);
+        int sueldoliquido = (int) (sueldobruto - sueldobruto * 0.006 - descuentoafp - descuentosalud);
+
+        System.out.println("El sueldo líquido es: " + sueldoliquido);
     }
 }
